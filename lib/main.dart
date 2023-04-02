c3ccbe4a2549c22140e92566714432ffaa49fd2b
kakelay <kakelay18052002@gmail.com>
2023-04-02 20:40:22 +0700

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,16 @@ import 'views/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
-  //Remove this method to stop OneSignal Debugging 
-OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
 
-OneSignal.shared.setAppId("daef386d-0ce7-4552-b889-ff9414e806be");
+  OneSignal.shared.setAppId("daef386d-0ce7-4552-b889-ff9414e806be");
 
 // The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
-OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    // ignore: avoid_print
     print("Accepted permission: $accepted");
-});
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -23,9 +24,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.pink
-    ));
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.pink));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
